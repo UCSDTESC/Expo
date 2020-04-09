8fd1ae717df71ab7c959078a1a14b0080a5ec91e
Kenneth Chen <efeichen@gmail.com>
2020-04-08 22:32:21 -0700

    Add initial styling to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,4 +2,9 @@ import React from "react"
 import styled from "styled-components"
 
 export default ({ children }) => <Footer>{children}</Footer>
-const Footer = styled.footer``
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1.2rem 0;
+`
